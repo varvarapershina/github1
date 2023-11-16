96bc867a768a5a82148eb418b7fec781a4c8706f
Varvara <pervaral@yandex.ru>
2023-11-16 17:22:19 +0300

    New

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    print('My first git program')
+    print('And I change it every day')
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('cfghthhhh')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
